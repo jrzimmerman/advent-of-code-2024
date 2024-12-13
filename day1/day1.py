ab9f92a214a0504a330d816d632ee21bdc446303
Justin Zimmerman <zimmy130@gmail.com>
2024-12-12 21:56:57 -0500

    day 1

---
+++ day1/day1.py
@@ -0,0 +1,41 @@
+
+lines = []
+with open("./input.txt","r") as f:
+    lines = f.readlines()
+
+count = 0
+left = []
+right = []
+for line in lines:
+    st = line.strip()
+    # print(st)
+    sp = st.split()
+    # print(sp)
+
+    left.append(int(sp[0]))
+    right.append(int(sp[1]))
+
+left = sorted(left)
+right = sorted(right)
+for i in range(len(left)):
+    l = left[i]
+    r = right[i]
+    count += abs(r-l)
+# print(count)
+
+map = {}
+
+for j in range(len(left)):
+    map[left[j]] = 0
+
+print(map)
+
+for k in range(len(right)):
+    print(right[k])
+    if right[k] in map:
+        map[right[k]] += 1
+
+ans = 0
+for key in map:
+    ans += key * map[key]
+print(ans)
